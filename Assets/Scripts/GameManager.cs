f46334d79accdf71daa56a840d42edcf49827799
zeldasouta-a11y <zeldasouta@gmail.com>
2025-10-11 23:44:26 +0900

    基本のゲームが遊べるようになりました
    的の生成ができるようになった。
    フルオートモードができるようになった。

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    [SerializeField] Vector3 minPosition = new Vector3(-15, 0, 20);
+    [SerializeField] Vector3 maxPosition = new Vector3(15, 10, 50);
+    [SerializeField] private int score = 0;
+    [SerializeField] private float createDuration = 1.0f;
+    [SerializeField] private CreateTargetManager _createTargetManager;
+    [SerializeField] TextMeshProUGUI scoreText
+    ;
+    private float timer = 0.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < createDuration )
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Vector3 pos = new Vector3(Random.Range(minPosition.x, maxPosition.x), Random.Range(minPosition.y, maxPosition.y), Random.Range(minPosition.z, maxPosition.z));
+            _createTargetManager.CreateInstanceAndSetCameraAndScripts(0, pos);
+            timer = 0.0f;
+        }
+    }
+    public void AddScore(int point)
+    {
+        score += point;
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score.ToString();
+        }
+        Debug.Log("Score: " + score);
+    }
+
+}
--- Assets/Scripts/TargetCollisionManager.cs
+++ Assets/Scripts/TargetCollisionManager.cs
@@ -1,27 +1,46 @@
 using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class TargetCollisionManager : MonoBehaviour
 {
-    private int hitcount = 0;
-    [SerializeField] CameraManager cameraManager;
+    [SerializeField] private int hitcount = 0;
+    [SerializeField] public GameObject targetModel;
+    [SerializeField] GameObject pointCanvas;
+    [SerializeField] private float vanishTime = 30.0f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (pointCanvas == null)
+        {
+            Debug.LogError("PointCanvas is not assigned in the inspector.");
+        }
+        if (targetModel == null)
+        {
+            Debug.LogError("TargetModel is not assigned in the inspector.");
+        }
+        pointCanvas.SetActive(false);
+        Destroy(this.gameObject, vanishTime);
+    }
     void OnTriggerEnter(Collider collision)
     {
         Debug.Log("Trigger Detected");
         string objecttag = collision.gameObject.tag;
         if (objecttag == "bullet")
         {
-            hitcount = 100;
-            GameObject hitclone = cameraManager.CreateInstance(this.transform.position+ new Vector3(0, 0.0f, 100.0f));
-            TextMeshProUGUI hittext = hitclone.GetComponentInChildren<TextMeshProUGUI>();
+            targetModel.SetActive(false);
+            TextMeshProUGUI hittext = pointCanvas.GetComponentInChildren<TextMeshProUGUI>();
             if (hittext == null)
             {
-                hittext = hitclone.AddComponent<TextMeshProUGUI>();
+                hittext = pointCanvas.AddComponent<TextMeshProUGUI>();
             }
             hittext.text = hitcount.ToString();
-            //Destroy(this.gameObject);
-            Destroy(hitclone, 2.0f);
+            pointCanvas.SetActive(true);
+
+            GameManager.Instance.AddScore(hitcount);
+            
+            Destroy(this.gameObject,3.0f);
         }
     }
     void OnTriggerStay(Collider other)
