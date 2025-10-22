04db7431337f6bb72d3a78e4e23cbb53688134c9
syacapachi <mountainside200@gmail.com>
2025-10-23 05:12:59 +0900

    fix_margeConflict
    マージの際消えた情報を修正

---
+++ Assets/Scripts/VRTickerTest/XRActionReader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class XRActionReader : MonoBehaviour
+{
+    public InputActionReference triggerAction; // Inspectorでアサイン
+
+    private void OnEnable()
+    {
+        triggerAction.action.performed += OnTriggerPerformed;
+        triggerAction.action.canceled += OnTriggerCanceled;
+        triggerAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        triggerAction.action.performed -= OnTriggerPerformed;
+        triggerAction.action.canceled -= OnTriggerCanceled;
+        triggerAction.action.Disable();
+    }
+
+    private void OnTriggerPerformed(InputAction.CallbackContext ctx)
+    {
+        float value = ctx.ReadValue<float>();
+        Debug.Log("Trigger pressed: " + value);
+        // イベントを発火するなど
+        OnTriggerValueChanged?.Invoke(value);
+    }
+
+    private void OnTriggerCanceled(InputAction.CallbackContext ctx)
+    {
+        OnTriggerValueChanged?.Invoke(0f);
+    }
+
+    public event System.Action<float> OnTriggerValueChanged;
+}
